861dec04a0e3c5b40f40097cf2f1bbd7bee1a38e
ybk1997 <yannickburkard1@gmail.com>
2026-05-13 15:05:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let width, height;
 let particles = [];
 let kuchens = [];
 const numParticles = 4000;
-const numKuchens = 200;
+const numKuchens = 300;
 const noiseScale = 0.002;
 const particleSpeed = 0.6;
 
